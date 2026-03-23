306f1b64c8eba5aba0f2ea9df54497fb9f7ecff7
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-23 08:25:15 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<PaletteDemo />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
