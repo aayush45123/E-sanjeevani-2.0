76875bae4598682af3f0474e4d53c9c0eb8bbd02
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-25 16:15:59 +0530

    initial commit

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Load user document
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) return res.status(401).json({ message: "User not found" });
+
+    req.user = user; // contains isAdmin also
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
