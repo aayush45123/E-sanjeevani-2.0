efc2d0f18576219a0dc656fda036ce815f664dd8
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-25 16:29:23 +0530

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+
+
+export default app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,7 @@
-console.log("Server is running...");
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
