467deb62261a3a4919eb0929a4b16eff0293e34f
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-25 16:31:47 +0530

    initial commit

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,71 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+//sign up ke liye 
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      isAdmin: false,
+    });
+
+    res.status(201).json({ message: "User created", user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
+export const me = async (req, res) => {
+  const user = req.user;
+
+  res.json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin === true || user.isAdmin === "true",
+  });
+};
+
+//login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
