d63910f9118bc90f8cb22aab241a19467f48a37f
Aayush Bharda <aayushbharda999@gmail.com>
2026-03-25 16:20:18 +0530

    initial commit

---
+++ server/src/routes/authRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { signup, login, me } from "../controllers/authController.js";
+import auth from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+router.get("/test", (req, res) => {
+  res.send("Auth route working!");
+});
+
+// Logged-in user
+router.get("/me", auth, async (req, res) => {
+  try {
+    const user = req.user; 
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      isAdmin: user.isAdmin === true,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
